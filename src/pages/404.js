da5cd651538ac6d6aa3f31e80bec81c78c87e8fd
Paul <40315177+PAXANDDOS@users.noreply.github.com>
2021-10-18 16:44:31 +0300

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,36 @@
-export const config = { amp: true }
+import Head from "next/head"
+import Link from "next/link"
+import nookies from "nookies"
+
+import Header from "../components/Base"
 
 const Custom404 = () => {
-	return <h1>404 - Page Not Found</h1>
+	return (
+		<>
+			<Head>
+				<title>Weevely</title>
+				<meta
+					name='viewport'
+					content='initial-scale=1.0, width=device-width'
+				/>
+			</Head>
+			<div>
+				<Header />
+				<h1>
+					Looks like you've lost! <Link href='/'>Go home.</Link>
+				</h1>
+			</div>
+		</>
+	)
+}
+
+export async function getServerSideProps(ctx) {
+	const user = nookies.get(ctx).user
+	if (!!user) {
+		ctx.res.writeHead(303, { Location: "/calendars" })
+		ctx.res.end()
+	}
+	return { props: {} }
 }
 
 export default Custom404
